78f44e795b575e0af00c13e9509b72efb8214a08
SaajidAhamed007 <saajidahamed01@gmail.com>
2025-07-09 18:44:33 +0530

    deploy error fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react"
 import { Navigate, Route,Routes } from "react-router-dom"
-import Login from "./components/login"
+import Login from "./components/Login"
 import Signup from "./components/Signup"
 import AdminDashboard from "./pages/admin/AdminDashboard"
 import AddEvent from "./pages/admin/AddEvent"
