5fd566b7d109a0df6091b2f6fa38cfb5606974aa
SaajidAhamed007 <saajidahamed01@gmail.com>
2025-07-30 17:46:14 +0530

    .env update

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,7 +5,7 @@ import { getStorage } from "firebase/storage"
 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyC3MMZNGSxoc0E4N2LEmv0f1ZDN4LUmefw",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
